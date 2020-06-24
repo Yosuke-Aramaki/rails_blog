4edce0452450b52e4be57988fc6dbde3c6d30668
Yosuke-Aramaki <gnx.vw903@gmail.com>
2020-06-25 08:26:15 +0900

    add ajax for destroy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -39,7 +39,10 @@ class PostsController < ApplicationController
   def destroy
     @post = Post.find(params[:id])
     @post.destroy
-    redirect_to posts_path
+    respond_to do |format|
+      format.js
+      format.html { redirect_to posts_path }
+    end
   end
   
   private
